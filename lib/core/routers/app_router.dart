219f4f3ef5f564d696682e48ed30890ef143f684
Dina Ahmed <denaahmed340@gmail.com>
2025-04-27 12:50:05 +0300

    finish on boarding

--- lib/core/routers/app_router.dart
+++ lib/core/routers/app_router.dart
@@ -6,6 +6,7 @@ import 'package:skin_sight_ai/core/routers/routing.dart';
 import 'package:skin_sight_ai/features/auth/presentation/views/sign_in_view.dart';
 import 'package:skin_sight_ai/features/auth/presentation/views/sign_up_view.dart';
 import 'package:skin_sight_ai/features/home/presentation/view/home_view.dart';
+import 'package:skin_sight_ai/features/onBoarding/presentation/view/onBoarding_view.dart';
 
 class AppRouter {
   static Route? generateRoute(RouteSettings routeSettings) {
@@ -14,7 +15,7 @@ class AppRouter {
       case Routing.splash:
         return _buildRoute(const Scaffold());
       case Routing.onboarding:
-        return _buildRoute(const Scaffold());
+        return _buildRoute(const OnboardingView());
       case Routing.signIn:
         return _buildRoute(const SignInView());
       case Routing.signUp:
--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -2,5 +2,7 @@
 class AppImages {
   AppImages._();
 
+  static String onBoardingImg = 'assets/images/onBoarding.png';
+  static String arrowRight = 'assets/images/arrow_right.png';
 }
 
